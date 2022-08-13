a2b560de942650c0d684154a57e2f69543e21c5a
nijuyonkadesu <24ksriramv@gmail.com>
2022-08-13 14:31:06 +0530

    fetch next word

--- app/src/main/java/com/example/android/unscramble/ui/game/GameViewModel.kt
+++ app/src/main/java/com/example/android/unscramble/ui/game/GameViewModel.kt
@@ -4,17 +4,58 @@ import android.util.Log
 import androidx.lifecycle.ViewModel
 
 class GameViewModel: ViewModel() {
-    init {
+    init { // to initialize lateinit properties when class created for the first time
         Log.d("GameFragment", "GameViewModel created")
+        getNextWord()
     }
+    private var wordsList: MutableList<String>? = mutableListOf()
+    private lateinit var currentWord: String
     private var score = 0
-    private var currentWordCount = 0
-    private var _currentScrambledWord = "test"
+    private var _currentWordCount = 0
+    private lateinit var _currentScrambledWord: String
     val currentScrambledWord: String
-        get() = _currentScrambledWord
+        get() = _currentScrambledWord  // backing
 
     override fun onCleared() {
         super.onCleared()
         Log.d("GameFragment", "GameViewModel is destroyed")
     }
+    private fun getNextWord(){
+        do{
+            currentWord = allWordsList.random()
+        } while (wordsList?.contains(currentWord) == true)
+        wordsList?.add(currentWord)
+        ++_currentWordCount
+        do {
+            val tempWord = currentWord.toCharArray()
+            tempWord.shuffle()
+            _currentScrambledWord = String(tempWord)
+        } while (_currentScrambledWord == currentWord)
+    }
+    private fun nextWord(): Boolean{
+        return if (_currentWordCount < MAX_NO_OF_WORDS) {
+            getNextWord()
+            true
+        }else false
+    }
 }
+
+/*
+    private fun getNextWord() {
+        currentWord = allWordsList.random()
+        val tempWord = currentWord.toCharArray()
+        tempWord.shuffle()
+        Log.d("GameFragment", tempWord.contentToString())
+
+        while (String(tempWord).equals(currentWord, false)) {
+            tempWord.shuffle()
+        }
+        if (wordsList?.contains(currentWord) == true) {
+            getNextWord()
+        } else {
+            _currentScrambledWord = String(tempWord)
+            ++_currentWordCount
+            wordsList?.add(currentWord)
+        }
+    }
+*/
